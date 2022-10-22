5a6b1e4ee96d43e33964a3fe8b866bab9a9be814
Saurav Singh <snipextt@gmail.com>
2022-10-23 00:33:53 +0530

    Default port

--- src/index.ts
+++ src/index.ts
@@ -5,5 +5,8 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 connect().then(() => {
-  app.listen(8000);
+  const port = process.env.PORT || 3000;
+  app.listen(port, () => {
+    console.log(`Server is listening on port ${port}`);
+  });
 });
